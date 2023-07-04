542a66f075abbdb64c043fc93c64c21637ecbd0a
MSI\gn01792218 <gn01792218@hotmail.com>
2023-07-04 16:17:37 +0800

    add delete project route

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,21 +1,36 @@
 //引入modles和repositpry
-import { Project } from '../models/project'
-import { ProjectRepository } from "../repositories/projectRepository"
-import { Request, Response } from 'express'
+import { Project } from "../models/project";
+import { ProjectRepository } from "../repositories/projectRepository";
+import { Request, Response } from "express";
 
 //初始化
-const projectRepository = new ProjectRepository()
+const projectRepository = new ProjectRepository();
 
-export const getProjectHomePage = (req:Request, res:Response)=>{
-    res.render('admin/project/ProjectHome',{pageTitle:"ProjectHome",projects:projectRepository.getAll(),layout:'layouts/adminLayout'})
-}
+export const getProjectHomePage = (req: Request, res: Response) => {
+  res.render("admin/project/ProjectHome", {
+    pageTitle: "ProjectHome",
+    projects: projectRepository.getAll(),
+    layout: "layouts/adminLayout",
+  });
+};
 
-export const getCreateProjectPage = (req:Request, res:Response) => {
-    res.render('admin/project/CreateProject',{pageTitle:"CreateProject",layout:'layouts/adminLayout'})
-}
+export const getCreateProjectPage = (req: Request, res: Response) => {
+  res.render("admin/project/CreateProject", {
+    pageTitle: "CreateProject",
+    layout: "layouts/adminLayout",
+  });
+};
 
-export const postCreateProjectPage = (req:Request, res:Response) => {
-    const project:Partial<Project> = req.body
-    projectRepository.add(project)
-    res.redirect('/admin/project')
-}
+export const postCreateProjectPage = (req: Request, res: Response) => {
+  const project: Partial<Project> = {
+    id: Math.random().toString(),
+    ...req.body,
+  };
+  projectRepository.add(project);
+  res.redirect("/admin/project");
+};
+
+export const deleteProject = (req: Request, res: Response) => {
+  projectRepository.delete(req.params.id);
+  res.redirect("/admin/project");
+};
--- src/routes/admin/project.ts
+++ src/routes/admin/project.ts
@@ -2,13 +2,16 @@ import { Router } from 'express'
 import { 
     getProjectHomePage,
     getCreateProjectPage,
-    postCreateProjectPage
+    postCreateProjectPage,
+    deleteProject
 } from'../../controllers/projectController'
 
 const router = Router()
 
 router.get('/', getProjectHomePage)
 
-router.get('/createProject',getCreateProjectPage)
-router.post('/createProject',postCreateProjectPage)
+router.get('/create', getCreateProjectPage)
+router.post('/create', postCreateProjectPage)
+
+router.get('/delete/:id', deleteProject)
 export default router
